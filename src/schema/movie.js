2f4ccdd5c4e79a1329391cf305a2feccd9b8ac72
Teun van Boxtel <teunvboxtel@hotmail.nl>
2019-02-14 15:20:03 +0100

    Forgot to commit earlier

--- src/schema/movie.js
+++ src/schema/movie.js
@@ -2,17 +2,51 @@ import { makeExecutableSchema } from 'graphql-tools'
 
 const typeDefs = `
     type Query {
-        movie(id: ID): Movie
+        movies: [Movie]
+        movie(id: ID, imdb_id: String): Movie
+    }
+    type Mutation {
+        upvoteMovie (
+            movieId: Int!
+        ): Movie 
     }
 
-    type Movie {
+    type Mutation {
+        rateMovie (
+            movieId: Int!
+            rating: ReviewInput!
+        ): Movie
+    }
+
+    input ReviewInput {
+        value: Int!
+        comment: String!
+    }
+
+    interface Media {
+        type: ID!
+        title: String!
+        media_type: String!
+    }
+
+    type Movie implements Media {
         id: ID!
         budget(currency: Currency = EUR): Int
-        title: String
+        title: String!
+        media_type: String!
+        duration: Int!
+        box_office: Int!
         release_date: String
         production_companies: [ProductionCompany]
     }
 
+    type TVShow implements Media {
+        id: ID!
+        title: String!
+        media_type: String!
+        episodes: [Episode]!
+        running: Boolean
+    }
     type ProductionCompany {
         name: String,
         id: Int        
@@ -23,6 +57,9 @@ const typeDefs = `
         GBP
         USD
       }
+
+    union SearchResult = Movie | TVShow | TVShowEpisode | Company | Person
+
 `;
 
 const resolvers = {
